d03514811d93cd41f2789448ce8667373093b530
Malmeidaa <mcan100496@gmail.com>
2023-04-26 18:51:09 -0300

    exer1

---
+++ force-app/main/default/lwc/exer1LWCII/exer1LWCII.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+
+export default class Exer1LWCII extends LightningElement {
+    pessoa = {
+        primeironome:"Mauricio",
+        sobrenome:"Almeida",
+        idade:27,
+        email:"mcan100496@gmail.com",
+        classificacao:"High",
+        empresa:"SoulCode",
+        receitaanual:"1.500.000,00"
+    }
+}
